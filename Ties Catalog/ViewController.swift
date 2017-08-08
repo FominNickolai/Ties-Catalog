cdab34727375389bfae510973b5a15905e62adfa
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-08 15:44:38 +0300

    Completed UI. Add Ties Data file.

--- Ties Catalog/ViewController.swift
+++ Ties Catalog/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOoutlets
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Actions
+    @IBAction func wear(_ sender: UIButton) {
+        
     }
-
-
+    @IBAction func rate(_ sender: UIButton) {
+        
+    }
+    
 }
 
